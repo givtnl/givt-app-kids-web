eea72be343fbaf5f34a9343e80afd7c75956e58d
Daniela Damaschin <whateverellie@gmail.com>
2023-07-04 12:23:00 +0200

    images and tags update

--- lib/features/recommendation/models/quiz_question.dart
+++ lib/features/recommendation/models/quiz_question.dart
@@ -1,6 +1,8 @@
+import 'package:givt_app_kids_web/features/recommendation/models/quiz_question_option.dart';
+
 class QuizQuestion {
   final String question;
-  final List<Map<String, String>> options;
+  final List<QuizQuestionOption> options;
 
   QuizQuestion({
     required this.question,
@@ -10,69 +12,26 @@ class QuizQuestion {
   static QuizQuestion whereDoYouWantToHelp = QuizQuestion(
     question: 'Where would you like to help?',
     options: [
-      {
-        'title': 'In my community',
-        'image': 'images/flags/netherlands.png',
-      },
-      {
-        'title': 'In my country',
-        'image': 'images/flags/india.png',
-      },
-      {
-        'title': 'All around the world',
-        'image': 'images/flags/question.png',
-      },
+      QuizQuestionOption.whereCommunity,
+      QuizQuestionOption.whereCountry,
+      QuizQuestionOption.whereWorld,
     ],
   );
-  static QuizQuestion helpPeople =
-      QuizQuestion(question: 'I want to help people...', options: [
-    {
-      'title': 'learn to read',
-      'image': 'images/flags/netherlands.png',
-    },
-    {
-      'title': 'protect animals',
-      'image': 'images/flags/india.png',
-    },
-    {
-      'title': 'stay healthy',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'care for children',
-      'image': '',
-    },
-    {
-      'title': 'go to school',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'with disabilities',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'that are homeless',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'get food',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'find a home',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'protect the forests',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'after a disaster',
-      'image': 'images/flags/question.png',
-    },
-    {
-      'title': 'clean oceans',
-      'image': 'images/flags/question.png',
-    },
-  ]);
+  static QuizQuestion helpPeople = QuizQuestion(
+    question: 'I want to help people...',
+    options: [
+      QuizQuestionOption.learnToRead,
+      QuizQuestionOption.helpAnimals,
+      QuizQuestionOption.helpHealthy,
+      QuizQuestionOption.careForChildren,
+      QuizQuestionOption.goToSchool,
+      QuizQuestionOption.withDisabilities,
+      QuizQuestionOption.thatAreHomeless,
+      QuizQuestionOption.getFood,
+      QuizQuestionOption.findaHome,
+      QuizQuestionOption.protectForests,
+      QuizQuestionOption.cleanOceans,
+      QuizQuestionOption.afteraDisaster
+    ],
+  );
 }
--- lib/features/recommendation/screens/quiz_screen.dart
+++ lib/features/recommendation/screens/quiz_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:givt_app_kids_web/features/recommendation/cubit/cubit/quiz_cubit.dart';
+import 'package:givt_app_kids_web/features/recommendation/models/quiz_question.dart';
 import 'package:givt_app_kids_web/features/recommendation/widgets/fab_recomendation.dart';
 import 'package:givt_app_kids_web/features/recommendation/widgets/givy_bubble.dart';
 
@@ -14,71 +15,90 @@ class WhereScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return BlocConsumer<QuizCubit, QuizState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
-      builder: (context, state) {
-        return Scaffold(
-          body: Container(
-            width: double.infinity,
-            height: double.maxFinite,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("images/gradient.png"),
-                fit: BoxFit.cover,
-              ),
+    QuizQuestion whereQuestions = QuizQuestion.whereDoYouWantToHelp;
+    QuizQuestion helpQuestions = QuizQuestion.helpPeople;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.maxFinite,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/gradient.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            GivyBubble(
+              text: whereQuestions.question,
             ),
-            child: Column(
-              children: [
-                GivyBubble(
-                  text: state.questions[0].question,
+            CarouselSlider(
+                carouselController: _controller,
+                options: CarouselOptions(
+                  height: size.height * 0.8,
+                  viewportFraction: 1,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: false,
+                  scrollDirection: Axis.vertical,
+                  //scrollPhysics: NeverScrollableScrollPhysics(),
                 ),
-                CarouselSlider(
-                    carouselController: _controller,
-                    options: CarouselOptions(
-                      height: size.height * 0.8,
-                      enlargeCenterPage: true,
-                      enableInfiniteScroll: false,
-                      scrollDirection: Axis.vertical,
-                      scrollPhysics: NeverScrollableScrollPhysics(),
+                items: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: whereQuestions.options
+                          .map((item) => Container(
+                                height: size.height * 0.3,
+                                width: size.width * 0.25,
+                                padding: EdgeInsets.all(size.width * 0.005),
+                                child: Card(
+                                    elevation: 25,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    color: Color(0XFFFAF4D8),
+                                    child: Column(
+                                      children: [
+                                        SvgPicture.asset(item.image),
+                                        Text(item.title)
+                                      ],
+                                    )),
+                              ))
+                          .toList()),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 200),
+                    child: GridView.count(
+                      childAspectRatio: 2,
+                      crossAxisCount: 4,
+                      children: helpQuestions.options
+                          .map((item) => SizedBox(
+                              width: 100,
+                              child: Card(
+                                  child: Row(
+                                children: [
+                                  Text(item.title),
+                                  SvgPicture.asset(item.image),
+                                ],
+                              ))))
+                          .toList(),
+
+                      // widget with results
                     ),
-                    items: [
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: state.questions[0].options
-                              .map((item) => Card(child: Text(item['title']!)))
-                              .toList()),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 200),
-                        child: GridView.count(
-                          childAspectRatio: 2,
-                          crossAxisCount: 4,
-                          children: state.questions[1].options
-                              .map((item) => SizedBox(
-                                  width: 100,
-                                  child: Card(child: Text(item['title']!))))
-                              .toList(),
-                        ),
-                      )
-                    ]),
-                Spacer(flex: 1),
-              ],
-            ),
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: RecommendationFAB(
-            text: 'Next',
-            onClicked: () {
-              _controller.nextPage();
-              // if (state is QuizWhere) {
-              //   context.read<QuizCubit>().nextInterests();
-              // }
-            },
-          ),
-        );
-      },
+                  )
+                ]),
+            Spacer(flex: 1),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: RecommendationFAB(
+        text: 'Next',
+        onClicked: () {
+          _controller.nextPage();
+          // if (state is QuizWhere) {
+          //   context.read<QuizCubit>().nextInterests();
+          // }
+        },
+      ),
     );
   }
 }
